b1c80e2b0011aed645cd6995d40b4070eb06b087
HilaryLanham <h26200@hotmail.com>
2021-10-01 11:51:53 +0100

    this has an error and i am not sure it actually works

--- app.test.js
+++ app.test.js
@@ -17,20 +17,40 @@ describe('GET requests', () => {
 
 });
 
-// TEST THE REST API ENDPOINT FOR PUT
-describe('CREATE request', () => {
+// TEST THE REST API ENDPOINT FOR POST
+//describe('CREATE request', () => {
     
-    test('CREATE product test', async () => {
-	// TEST IN HERE
+    // test('CREATE product test', async () => {
+    //     const res = await request(app).post('/product/create')
+    //     expect(res.statusCode).toBe(201);
+    // });
+
+    describe('CREATE request', () => {
+    
+        test('CREATE product test', async () => {
+            const res = await request(app)
+                                .post('/product/create')
+                                .send({
+                                    name : "test name", 
+                                    description : "test desc",
+                                    price : 0
+                                });
+            expect(res.statusCode).toBe(201);
+        });
+    
+    
     });
 
-});
 
-// UNIT TEST THE PRODUCT BUILDER
-describe('Unit Tests', () => {
-
-    test('product object builder', () => {
-        // TEST IN HERE
-    });
-
-});
+    // UNIT TEST THE PRODUCT BUILDER
+describe('Unit Tests', () => {​​​
+letobj = {​​​
+name:"name",
+description:"desc",
+price:15.99
+    }​​​;
+test('product object builder', () => {​​​
+// TEST IN HERE
+expect(productBuilder("name", "desc", 25.99)).toEqual(obj);
+    }​​​);
+}​​​);
